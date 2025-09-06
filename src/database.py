71229e13014ceb798d9d346535636b43a6f925c7
Artem Somar <136079772+artemsomar@users.noreply.github.com>
2025-09-06 16:28:45 +0300

    Revert "Added data reading from database. Added embedding with chunks."
    This reverts commit fefa07ade98d96891db5f41303df256e18b6da6e.

--- src/database.py
+++ src/database.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, asyn
 from src.config import settings
 
 
+
 db_engine = create_async_engine(
     url=settings.db.url,
     echo=settings.db.echo,
